fed67ab73eb9a1c7a56dcd1d606f493dde1f75ee
G2 <g2simm+FromSourceTree@gmail.com>
2014-09-26 00:57:48 +0900

    first commit

---
+++ FaceEditor/Model/CameraCapture.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using OpenCvSharp;
+using System.Windows.Threading;
+using System.Windows.Media.Imaging;
+using OpenCvSharp.Extensions;
+using System.Windows.Media;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace AvalonEdit.Sample.Model
+{
+    public class CameraCapture : ViewModel.ViewModelBase
+    {
+        private DispatcherTimer cameraTimer;
+        private CvCapture capture;
+//        public WriteableBitmap Capture { get; set; }
+
+        private WriteableBitmap _capture;
+        public WriteableBitmap Capture
+        {
+            get { return _capture; }
+            set
+            {
+                _capture = value;
+                OnPropertyChanged("Capture");
+            }
+        }
+        public void Start(int cameraIndex){
+            this.Start(cameraIndex,10);
+        }
+
+        public void Start(int cameraIndex,int timeSpan)
+        {
+            try
+            {
+                capture = Cv.CreateCameraCapture(cameraIndex);
+            }
+            catch (Exception e)
+            {
+                App.ShowErrorMessage("カメラを認識できませんでした。");
+                return;
+            }
+            var wb = new WriteableBitmap(capture.FrameWidth, capture.FrameHeight, 96, 96, PixelFormats.Bgr24, null);
+            cameraTimer = new DispatcherTimer(DispatcherPriority.Background);
+            cameraTimer.Interval = TimeSpan.FromMilliseconds(timeSpan);
+            cameraTimer.Tick += ((sender, e) =>
+            {
+                WriteableBitmapConverter.ToWriteableBitmap(capture.QueryFrame(), wb);
+                Capture = wb;
+            });
+            cameraTimer.Start();
+        }
+        public void Stop()
+        {
+            cameraTimer.Stop();
+            capture.Dispose();
+        }
+
+        public void ReStart()
+        {
+            cameraTimer.Start();
+        }
+    }
+}
---
+++ FaceEditor/ViewModel/Window1ViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+using AvalonEdit.Sample.Model;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace AvalonEdit.Sample.ViewModel
+{
+    public class Window1ViewModel : ViewModelBase
+    {
+
+        private WriteableBitmap _capture;
+        public WriteableBitmap CaptureImage
+        {
+            get { return _capture; }
+            set
+            {
+                _capture = value;
+                OnPropertyChanged("CaptureImage");
+            }
+        }
+        private CameraCapture _camera;
+        public CameraCapture Camera
+        {
+            get
+            {
+                return _camera ?? (_camera = new CameraCapture());
+
+            }
+            set { _camera = value; }
+        }
+
+        private RelayCommand _cameraCommand;
+        public RelayCommand CameraCommand
+        {
+            get { return _cameraCommand ?? (_cameraCommand = new RelayCommand(CaptureStart)); }
+            set { _cameraCommand = value; }
+        }
+
+        public void CaptureStart(object obj)
+        {
+            Camera.PropertyChanged += (_sender,_e) =>
+            {
+                var cc = (CameraCapture)_sender;
+                switch (_e.PropertyName)
+                {
+                    case "Capture":
+                        CaptureImage = cc.Capture;
+                        break;
+                    default:
+                        break;
+                }
+
+            };
+            Camera.Start(0,1);
+        }
+
+        public void CaptureStop()
+        {
+            Camera.Stop();
+        }
+    }
+}
